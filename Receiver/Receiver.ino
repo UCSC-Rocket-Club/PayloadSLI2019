eb3ee2ed326d9f9aa24bc3bf5f97d4157885fe7e
eights <oliviaawongg@gmail.com>
2020-01-31 19:42:36 -0800

    changed led blink, pin

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -5,45 +5,57 @@
 // reliability, so you should only use RH_RF95 if you do not need the higher
 // level messaging abilities.
 // It is designed to work with the other example Feather9x_TX
- 
+
 #include <SPI.h>
 #include <RH_RF95.h>
 //#define DEBUG_MODE  0
-// When flight mode is enabled, do not run any serial code because it will not run anything else 
-// and waste energy. This will disable any serial logs, so 
+// When flight mode is enabled, do not run any serial code because it will not run anything else
+// and waste energy. This will disable any serial logs, so
 // use DEBUG_MODE 0 for debugging, and DEBUG_MODE 1 for actual flights
- 
+
 // for Feather32u4 RFM9x
 #define RFM95_CS 8
 #define RFM95_RST 4
 #define RFM95_INT 7
- 
+
 // Change to 434.0 or other frequency, must match RX's freq!
 #define RF95_FREQ 915.0
- 
+
 // Singleton instance of the radio driver
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
- 
+
 // Blinky on receipt
 #define LED 13
- 
+#define EMATCH_GATE 2
+
+void blinkLed() {
+  digitalWrite(LED, HIGH);
+  delay(400);
+  digitalWrite(LED, LOW);
+  delay(400);
+  digitalWrite(LED, HIGH);
+  delay(400);
+  digitalWrite(LED, LOW);
+  delay(400);
+}
+
 void setup()
 {
   pinMode(LED, OUTPUT);
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
-  
-  #ifdef DEBUG_MODE
+
+#ifdef DEBUG_MODE
   // initialize serial
   Serial.begin(115200);
   while (!Serial) {
     delay(1);
   }
   delay(100);
- 
+
   Serial.println("Feather LoRa RX Test!");
-  #endif
- 
+#endif
+
   // manual reset
   digitalWrite(RFM95_RST, LOW);
   delay(10);
@@ -54,23 +66,20 @@ void setup()
     ;
   }
 
-    delay(5000);
-    digitalWrite(LED, HIGH);
-    delay(5000);
-    digitalWrite(LED, LOW);
+  blinkLed();
 
-  #ifdef DEBUG_MODE 
+#ifdef DEBUG_MODE
   while (!rf95.init()) {
     Serial.println("LoRa radio init failed");
     Serial.println("Uncomment '#define SERIAL_DEBUG' in RH_RF95.cpp for detailed debug info");
     while (1);
   }
-  #endif
-  
-  #ifdef DEBUG_MODE
+#endif
+
+#ifdef DEBUG_MODE
   Serial.println("LoRa radio init OK!");
-  #endif
-  
+#endif
+
   // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
   //#ifdef DEBUG_MODE
   if (!rf95.setFrequency(RF95_FREQ)) {
@@ -83,12 +92,12 @@ void setup()
   }
   //#endif
 
-  #ifdef DEBUG_MODE
+#ifdef DEBUG_MODE
   Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
-  #endif
-  
+#endif
+
   // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
- 
+
   // The default transmitter power is 13dBm, using PA_BOOST.
   // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then
   // you can set transmitter powers from 5 to 23 dBm:
@@ -104,9 +113,9 @@ static uint8_t finishedDeployingMsg[] = "!! -- Finished Deployment -- !!";
 static uint8_t invalidInputMsg[] = "**-- Not a valid input,  send 'f' , 'y' , 'n' , or 'p'-- **";
 
 void loop() {
- 
 
-  if (rf95.available()) {    
+
+  if (rf95.available()) {
     // initialization for message array
     uint8_t buf[3];
     uint8_t len = sizeof(buf);
@@ -115,7 +124,7 @@ void loop() {
     if (rf95.recv(buf, &len) ) {
 
       Serial.println("------------------------------------");
-      
+
       // writing and printing data block
       digitalWrite(LED, HIGH);
       RH_RF95::printBuffer("Received: ", buf, len);
@@ -130,48 +139,48 @@ void loop() {
       Serial.println("------------------------------------");
 
       // ------------- input checking -------------
-      
+
       char* input = (char*)buf;
-        
-      switch(*input){
-         case 'f':
-             rf95.send(confirmFireMsg, sizeof(confirmFireMsg));
-             check = 0;
-             break;
 
-         case 'n':
-              rf95.send(abortingMsg, sizeof(abortingMsg));
-              break;
-             
-         case 'y':
-             if(check == 0){ 
-                rf95.send(deployingMsg, sizeof(deployingMsg));
-                digitalWrite(10,HIGH);
-                delay(2000);
-                digitalWrite(10,LOW);
+      switch (*input) {
+        case 'f':
+          rf95.send(confirmFireMsg, sizeof(confirmFireMsg));
+          check = 0;
+          break;
 
-                rf95.send(finishedDeployingMsg, sizeof(finishedDeployingMsg));
-                check = 1;
-             }
-             else
-                rf95.send(fireInstructionMsg, sizeof(fireInstructionMsg));
-             break;
-             
-         default:
-             //rf95.send(backbs, sizeof(backbs));
-             //check = 3;
-             rf95.send(invalidInputMsg, sizeof(invalidInputMsg));
-             break;
-         
+        case 'n':
+          rf95.send(abortingMsg, sizeof(abortingMsg));
+          break;
+
+        case 'y':
+          if (check == 0) {
+            rf95.send(deployingMsg, sizeof(deployingMsg));
+            digitalWrite(EMATCH_GATE, HIGH);
+            delay(2000);
+            digitalWrite(EMATCH_GATE, LOW);
+
+            rf95.send(finishedDeployingMsg, sizeof(finishedDeployingMsg));
+            check = 1;
+          }
+          else
+            rf95.send(fireInstructionMsg, sizeof(fireInstructionMsg));
+          break;
+
+        default:
+          //rf95.send(backbs, sizeof(backbs));
+          //check = 3;
+          rf95.send(invalidInputMsg, sizeof(invalidInputMsg));
+          break;
+
       }
       digitalWrite(LED, LOW);
     }
 
-    #ifdef DEBUG_MODE
+#ifdef DEBUG_MODE
     else {
       Serial.println("!! -- Receive failed -- !!");
     }
-    #endif
+#endif
 
 
   }
